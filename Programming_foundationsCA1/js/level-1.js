fe15c991f11e2d84a87d6a6250c2ed94afa4e28c
vivijg <57644737+vivijg@users.noreply.github.com>
2020-01-19 13:02:09 +0100

    Add files via upload

---
+++ Programming_foundationsCA1/js/level-1.js
@@ -0,0 +1,99 @@
+//task 1
+
+var color = "Night skyblue";
+console.log (color);
+
+
+
+// Task 2
+var person = {
+name: "Vivian Grosch",
+age:25
+};
+
+console.log (person);
+
+
+
+//Task 3
+var outOfStock = true;
+
+
+if (outOfStock === true){
+    console.log("true");
+    }
+else{
+    console.log("Out of stock");
+   }
+
+
+//Task 4
+
+var count= (1,2,3,4,5)
+for (var count=1; count<=5; count++){
+console.log (count);
+}
+
+//task 5
+
+for (var i=15; i<=25; i++){
+    console.log (i);
+}
+
+//task 6
+for (var i=15; i<=25; i++){
+  
+if (i===20){
+    console.log (i);
+}
+}
+
+//task 7
+var foodObject = {
+    quantity: 6,
+    name: "apple",
+    color: "red"
+
+    };
+
+    console.log (foodObject.quantity)
+
+
+    if (foodObject.name === true){
+        console.log(foodObject.name);
+        }
+    else{
+        console.log("6 Red apples");
+       }
+  
+    
+  
+    
+
+
+
+//task 8
+function whatIDontLike (TastlessGreens)
+{
+    console.log(TastlessGreens);
+};
+
+whatIDontLike ("I dont like brokkoli");
+
+//task 9
+
+
+
+ function subtractNumbers (number1, number2){
+     var sum = number1- number2;
+     console.log (sum);
+ }
+ subtractNumbers (28,8)
+
+//task 10
+var emptyArray = [];
+function logToConsole (wineTime){
+console.log (wineTime);
+}
+
+logToConsole("It is Wine time");
